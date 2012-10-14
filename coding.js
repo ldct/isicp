d89de837ca7d23d53ae26c06df0e718aa935fb18
Li Xuanji <xuanji@gmail.com>
2012-10-14 12:55:13 +0800

    create excercise code

--- coding.js
+++ coding.js
@@ -10,7 +10,7 @@ function cleanCode(code) {
   return code.replace(/^\n/, "").replace(/\n*$/, "").replace(/\s*\n/g, "\n");
 }
 
-function createPrompt(target_string) {
+function createPrompt(target_string, answer) {
   var target = $(target_string);
   var code = cleanCode(target.text());
   console.log(target_string, code);
@@ -18,11 +18,23 @@ function createPrompt(target_string) {
   var form = $("<textarea>", {text: code});
   target.append(form);
   
+  
+  if (answer) {
+    grade = $("<div />", {id: target_string.replace("#", "") + "-grade", text: 'hi'});
+    $(target_string + "-output").after(grade);
+  }
+  
   function update() {
     var str = editor.getValue();
     target.find(".output").empty();
     result = biwascheme.evaluate(str);
     $(target_string + "-output").empty().append($("<span>" + result + "</span>"));
+    
+    if (answer && answer == result) {
+      $(target_string + "-grade").attr({'class': 'correct-answer'}).text('\u2713');
+    } else {
+      $(target_string + "-grade").attr({'class': 'wrong-answer'}).text('\u2717');
+    }
   }
   
   var editor = CodeMirror.fromTextArea(form[0],
@@ -31,4 +43,13 @@ function createPrompt(target_string) {
     "onBlur": update
   });
   update();
+}
+
+function attachGrading(target_string, answer) {
+
+
+  $("#" + target_string).change(function() {
+    alert("change!")
+
+  });
 }
