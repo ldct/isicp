67a26d742ee7c41ccb6dbc5a19014aed10316acc
Li Xuanji <xuanji@gmail.com>
2012-11-03 22:05:00 +0800

    decouple answer creation, put jQuery objects into global scope

--- coding.js
+++ coding.js
@@ -12,50 +12,68 @@ function cleanCode(code) {
   return code.replace(/^\n/, "").replace(/\n*$/, "").replace(/\s*\n/g, "\n").replace(/\s*$/, "");
 }
 
-function createPrompt(target_string, answer) {
+var domOf = {};
+var answerOf = {};
+
+function init(target_string) {
+
+// check that $target exists, turn it into a $form, attach an $output
+// and add $target, $form, and $output to global scope
+
   var $target = $("#" + target_string);
   if (!$target[0]) {
-    console.error("$" + target_string + " did not match anything");
-    return;
+    throw "$" + target_string + " did not match anything";
   }
+  
   var code = cleanCode($target.text());
   var $form = $("<textarea>", {text: code});
   $target.empty().append($form);
+  
   var $output = $("#" + target_string + "-output")
   if (!$output[0]) {
     $output = $("<div />", {id: target_string + "-output", 'class':'output'});
     $target.after($output);
   }
-  if (answer) {
-    var $grade = $("<div />", {id: target_string + "-grade", text: 'hi'});
-    $output.after($grade);
-  }
+  
+  domOf[target_string] = {'$target': $target, '$form': $form, '$output': $output};
+}
+
+function attachAnswer(target_string, answer) {
+  answerOf[target_string] = answer;
+  var $grade = $("<div />", {id: target_string + "-grade", text: 'hi'});
+  domOf[target_string]['$output'].after($grade);
+  domOf[target_string]['$grade'] = $grade;
+  $grade.attr({'class': 'wrong-answer'}).text('\u2717');
+}
+
+function createPrompt(target_string) {
+  
+  init(target_string);
   
   var biwascheme = new BiwaScheme.Interpreter( function(e){
     console.log(e.message);
   });
-  
-  console.log(target_string, biwascheme);
 
   function update() {
-    var str = editor.getValue();
-    result = biwascheme.evaluate(str);
-    $output.empty().append($("<span>" + result + "</span>"));
+    result = biwascheme.evaluate(editor.getValue());
+    domOf[target_string]["$output"].empty().append($("<span>" + result + "</span>"));
     
-    if (answer) {
+    if (answer = answerOf[target_string]) {
+      var $grade = domOf[target_string]['$grade'];
       if (answer == result) {
         $grade.attr({'class': 'correct-answer'}).text('\u2713');
       } else {
         $grade.attr({'class': 'wrong-answer'}).text('\u2717');
       }
     }
-    console.log(biwascheme);
   }
   
-  var editor = CodeMirror.fromTextArea($form[0],
+  var editor = CodeMirror.fromTextArea(domOf[target_string]['$form'][0],
   {
     "matchBrackets": true, 
     "onBlur": update
   });
   update();
 }
+
+console.log(domOf);
