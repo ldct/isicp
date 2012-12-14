a9df96bcf8ec755823e570915ca6598e1f944474
Andres Raba <sarabanderest@gmail.com>
2012-12-14 12:31:36 +0200

    refactored footnote code

--- footnote/footnote.js
+++ footnote/footnote.js
@@ -1,9 +1,12 @@
-$(document).ready(function() {
-  $(".footnote").tooltip( { delay: 500 } )
-    .dynamic( {	
-		top:	  { direction: 'up' },
-		bottom:	  { direction: 'down' },
-		left:	  { direction: 'right' },
-		right:	  { direction: 'left' } 
-	      } );
-});
+// Binds tooltip to preceding element having class "footnote".
+var bind_footnote = function() {
+  $(document).ready( function() {
+    $(".footnote").tooltip({ delay: 500 })
+      .dynamic({	
+  		top:	  { direction: 'up' },
+  		bottom:	  { direction: 'down' },
+  		left:	  { direction: 'right' },
+  		right:	  { direction: 'left' } 
+  	       });
+  });
+};
