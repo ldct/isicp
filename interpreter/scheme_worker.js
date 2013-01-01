38ffca22a8ddbdc9b4b7e2bfbe1264151aadb69f
Li Xuanji <xuanji@gmail.com>
2013-01-01 14:21:49 +0800

    fix some bugs in scheme_worker.js

--- interpreter/scheme_worker.js
+++ interpreter/scheme_worker.js
@@ -4,30 +4,13 @@ onmessage = function(event) {
   
   var env = create_global_frame();
   var codebuffer = new Buffer(tokenize_lines([event.data]));
-  this.postMessage(scheme_eval(scheme_read(codebuffer), env));
-
-/*
-  var args=event.data.args;
-  switch(event.data.op) {
-    case "+":
-      var sum=0;
-      for (var i in args) {
-        sum += args[i];
-      }
-      this.postMessage(sum);
-      break;
-    case "*":
-      var prod=1;
-      for (var i in args) {
-        prod *= args[i];
-      }
-      this.postMessage(prod);
-      break;
-    case "display":
-      for (var i=0, length=args.length; i<length;i++) {
-        this.postMessage(args[i]);
-      }
-      break;
+  
+  while (codebuffer.current() != null) {
+	
+    var result = scheme_eval(scheme_read(codebuffer), env);
+    if (! (result === null || result === undefined)) {
+	  this.postMessage(result.toString());
+    }
   }
-*/
+  
 };
