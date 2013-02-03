9f023304c9939b36f35ad517bc4cf526895a1162
Li Xuanji <xuanji@gmail.com>
2013-02-03 21:47:20 +0800

    add Y combinator, also add a way of printing in normal LC notation

--- js/lambda/lambda.js
+++ js/lambda/lambda.js
@@ -1,9 +1,12 @@
-function format(s, args) {
-  if (args.length === 0) {
-    return s;
-  } else {  
-    return format(s.replace("%s", args[0]), args.slice(1));
+function format(s) {
+  function helper(s, args) {
+    if (args.length === 0) {
+      return s;
+    } else {  
+      return helper(s.replace("%s", args[0]), args.slice(1));
+    }
   }
+  return helper(s, Array.prototype.slice.call(arguments).slice(1));
 }
 
 /*
@@ -11,28 +14,30 @@ Unbound variables ("constants") are represented as strings
 Bound variables are represented as numbers, interpreted as de brujin indexes
 */
 
-/*represents a lambda abstraction term*/
 function lambdaTerm(body) {
   this.body = body;
 }
 
 lambdaTerm.prototype.toString = function() {
-  return format("(%s %s)", ["\u03bb", this.body.toString()]);
+  return format("(λ %s)", this.body.toString());
 }
 
-lambdaTerm.prototype.type = function() {return "lambda"}
-
-/*represents a function application term*/
 function applicationTerm(func, body) {
   this.func = func;
   this.body = body;
 }
 
 applicationTerm.prototype.toString = function() {
-  return "(" + this.func.toString() + " " + this.body.toString() + ")";
+  return format("(%s %s)", this.func.toString(), this.body.toString());
 }
 
-applicationTerm.prototype.type = function() {return "application"}
+function l(body) {
+  return new lambdaTerm(body);
+}
+
+function a(func, body) {
+  return new applicationTerm(func, body);
+}
 
 function lift(k, n, term) {
   if (typeof term === "number") {
@@ -43,9 +48,9 @@ function lift(k, n, term) {
     }
   } else if (typeof term === "string") {
     return term;
-  } else if (term.type() === "application") {
+  } else if (term instanceof applicationTerm) {
     return new applicationTerm(lift(k, n, term.func), (lift(k, n, term.body)));
-  } else if (term.type() === "lambda") {
+  } else if (term instanceof lambdaTerm) {
     return new lambdaTerm(lift(k+1, n, term.body));
   } else {
     throw "lift called with invalid term " + term;
@@ -63,9 +68,9 @@ function sub(term, k, N) {
     }
   } else if (typeof term === "string") {
     return term;
-  } else if (term.type() === "lambda") {
+  } else if (term instanceof lambdaTerm) {
     return new lambdaTerm(sub(term.body, k + 1, N));
-  } else if (term.type() === "application") {
+  } else if (term instanceof applicationTerm) {
     return new applicationTerm(sub(term.func, k, N), sub(term.body, k, N));
   } else {
     throw "error: " + term + " matched no types";
@@ -81,13 +86,13 @@ function simplify(term) {
     return term;
   } else if (typeof term === "string") {
     return term;
-  } else if (term.type() === "lambda") {
+  } else if (term instanceof lambdaTerm) {
     return new lambdaTerm(simplify(term.body));
-  } else if (term.type() === "application") {
+  } else if (term instanceof applicationTerm) {
     var newfunc = simplify(term.func);
     var newbody = simplify(term.body);
     
-    if (newfunc.type && newfunc.type() == "lambda") {
+    if (newfunc instanceof lambdaTerm) {
       return apply_lambda(newfunc, newbody);
     } else {
       return new applicationTerm(newfunc, newbody);
@@ -95,4 +100,40 @@ function simplify(term) {
   } else {
     throw "error: " + term + " matched no types";
   }
+}
+
+function to_named_string(term) {
+  function helper(term, fresh_vars) {
+    if (fresh_vars.length == 0) {
+      throw "run out of fresh variables";
+    }
+    if (typeof term === "number") {
+      return String(term);
+    } else if (typeof term === "string") {
+      return term;
+    } else if (term instanceof lambdaTerm) {
+      var fresh = fresh_vars[0];
+      return format("λ%s.%s", fresh, helper(apply_lambda(term, fresh), fresh_vars.slice(1)));
+    } else if (term instanceof applicationTerm) {
+      var new_func = helper(term.func, fresh_vars);
+      var new_body = helper(term.body, fresh_vars)
+      
+      if (term.func instanceof lambdaTerm) {
+        var left = format("(%s)", new_func);
+      } else {
+        var left = new_func;
+      }
+      
+      if ((term.body instanceof applicationTerm) || (term.body instanceof lambdaTerm)) {
+        var right = format("(%s)", new_body);
+      } else {
+        var right = new_body;
+      }
+      
+      return left + right;
+    } else {
+      throw "error: " + term + " matched no types";
+    }
+  }
+  return helper(term, ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p"]).replace(/\.λ/g, "");
 }
