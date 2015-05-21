bd1aee2b139a748aa99e5930567d933246b1d0c0
gilch <githuub.gilch@xoxy.net>
2015-05-21 16:55:58 -0600

    force https for mathjax and raphael-min
    Local files render properly in Firefox now.

--- render.py
+++ render.py
@@ -3,14 +3,14 @@
 import sys, re
 
 tag_re = re.compile(
-  r"^({{[\w@]+}})$"  # (Capture) {{Tag}} words
-  r"(.*?)"  # (Capture) content lines until
-  r"^@@$",  # terminating @@ line
-  flags=re.DOTALL|re.MULTILINE)  # . includes \n | ^ and $ are per line.
+  r"^({{[\w@]+}})$"  # ()Capture {{Tag}} word,
+  r"(.*?)"  # then ()capture characters until
+  r"^@@$",  # terminating "@@" line.
+  flags=re.DOTALL|re.MULTILINE)  # '.' includes '\n' | '^' and '$' are per line.
 
 with open(sys.argv[1]) as content:
   tag_content = dict(re.findall(tag_re, ''.join(content.readlines())))
 
 with open("template.html") as template:
   for L in template.readlines():
-    print tag_content.get(L.strip()) or L 
+    print tag_content.get(L.strip(), L)
